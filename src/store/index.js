76bfdf8ad6e84dc5847df0084f823499dd564a2e
Jtsant <jtsant92@hotmail.com>
2022-07-21 14:51:38 +0300

    Redux Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,21 @@
-import { configureStore } from 'redux';
+import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-        }
-    }
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-        }
-    }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    return state;
+  return state;
 };
 
-const store = configureStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
